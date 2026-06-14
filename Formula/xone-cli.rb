c85fef7f8cf14c2d5413b9555b08e678fa779d54
magic <wsz1143200@163.com>
2026-06-15 00:48:24 +0800

    chore: update xone cli formula to 0.1.2

--- Formula/xone-cli.rb
+++ Formula/xone-cli.rb
@@ -3,8 +3,8 @@ class XoneCli < Formula
 
   desc "Unified CLI entry point for X-One Agent Evidence Loop workflows"
   homepage "https://github.com/X-One-AI/xone-cli"
-  url "https://files.pythonhosted.org/packages/ec/ca/df5a51a92ffc5ddaf2c2f2fec3f14161dd7f8997dabeb634ee2f37a1ce47/xone_cli-0.1.1.tar.gz"
-  sha256 "07d07b2016756125d68f068b2f2cc517b48198207c34ee43b409133849b25554"
+  url "https://files.pythonhosted.org/packages/0d/f1/19d45b37cc47b7ea8a13ad258e374dd960e8efc51b7c768b8bf00d461806/xone_cli-0.1.2.tar.gz"
+  sha256 "b78c66d8bf202f552d8b446adf06645ee51611ffa91c2f5e80d18eb3c0376d40"
   license "MIT"
 
   depends_on "python@3.13"
@@ -14,7 +14,7 @@ class XoneCli < Formula
   end
 
   test do
-    assert_match "xone 0.1.1", shell_output("#{bin}/xone --version")
+    assert_match "xone 0.1.2", shell_output("#{bin}/xone --version")
     assert_match "X-One install plan", shell_output("#{bin}/xone doctor --install-plan")
     assert_match "xone.doctor.v1", shell_output("#{bin}/xone doctor --json")
   end
